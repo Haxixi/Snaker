25a6a7df41f1ca208717f61db3fead858e9678b3
nothing <287031517@qq.com>
2017-10-26 20:28:09 +0800

    开始UI框架

---
+++ Assets/Snaker/Service/UIManager/UIPage.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Snaker.Service.UIManager
+{
+
+    public class UIPage : UIPanel
+    {
+
+        [SerializeField] private Button m_btnGoBack;
+
+        protected object m_openArgs;
+
+        private bool m_isOpenedOnce;
+
+        protected void OnEnable()
+        {
+            Debug.Log("OnEnable  ");
+
+            if (m_btnGoBack != null)
+                m_btnGoBack.onClick.AddListener(OnBtnGoBack);
+
+#if UNITY_EDITOR
+            if (m_isOpenedOnce)
+                OnOpen(m_openArgs);
+#endif
+        }
+
+        protected void OnDisable()
+        {
+            Debug.Log("OnDisable  ");
+
+#if UNITY_EDITOR
+            if (m_isOpenedOnce)
+                OnClose();
+#endif
+
+            if (m_btnGoBack != null)
+                m_btnGoBack.onClick.RemoveAllListeners();
+        }
+
+        private void OnBtnGoBack()
+        {
+            Debug.Log("OnBtnGoBack  ");
+            UIManager.Instance.GoBackPage();
+        }
+
+        public sealed override void Open(object args = null)
+        {
+            Debug.Log("Override Open  ");
+            m_openArgs = args;
+            m_isOpenedOnce = false;
+
+            if(!this.gameObject.activeSelf)
+                this.gameObject.SetActive(true);
+
+            OnOpen(args);
+            m_isOpenedOnce = true;
+        }
+
+        public sealed override void Close()
+        {
+            Debug.Log("Override Close  ");
+            if(this.gameObject.activeSelf)
+                this.gameObject.SetActive(false);
+
+            OnClose();
+        }
+    }
+}
+
+
---
+++ Assets/Snaker/Service/UIManager/UIPanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Snaker.Service.UIManager
+{
+    public abstract class UIPanel : MonoBehaviour
+    {
+
+        public virtual void Open(object args = null)
+        {
+            Debug.Log("Open  " + args);
+        }
+
+        public virtual void Close()
+        {
+            Debug.Log("Close  ");
+        }
+
+        public bool IsOpen
+        {
+            get { return this.gameObject.activeSelf; }
+        }
+
+        protected virtual void OnClose()
+        {
+            Debug.Log("OnClose  ");
+        }
+
+        protected virtual void OnOpen(object args = null)
+        {
+            Debug.Log("OnOpen  " + args);
+        }
+    }
+}
+
+
